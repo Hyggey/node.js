025e5502761a4a962f2e5c77ec1b4f224f2e0af1
zhangzhen1897 <614564049@qq.com>
2020-05-14 16:35:46 +0800

    crud

--- 04/crud-express/app.js
+++ 04/crud-express/app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 
 var app = express()
+var fs = require('fs')
 
 app.engine('html', require('express-art-template'));
 
@@ -8,13 +9,21 @@ app.use('/node_modules/',express.static('./node_modules/'))
 app.use('/public/',express.static('./public/'))
 
 app.get('/',function(req,res){
-    res.render('index.html',{
-        title:[
-            '苹果',
-            "香蕉",
-            "梨子",
-            "西瓜"
-        ]
+    fs.readFile('./db.json','utf8',function(err,data){
+        if(err){
+            return res.status(500).send('server err')
+        }
+        console.log(data)
+        res.render('index.html',{
+            title:[
+                '苹果',
+                "香蕉",
+                "梨子",
+                "西瓜"
+            ],
+            students: JSON.parse(data).students
+    
+        })
     })
 })
 
